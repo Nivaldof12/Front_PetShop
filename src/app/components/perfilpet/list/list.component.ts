6ad0b73119471fae28933b264843000dfc7ce2ad
Nivaldof12 <nivaldopvp@gmail.com>
2024-02-28 19:09:06 -0300

    Cadastro perfil Pet criado

--- src/app/components/perfilpet/list/list.component.ts
+++ src/app/components/perfilpet/list/list.component.ts
@@ -2,6 +2,7 @@ import { NgTemplateOutlet } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { PerfilPet } from 'src/app/models/perfilpet';
 
 @Component({
@@ -24,7 +25,7 @@ export class ListComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
   dataSource = new MatTableDataSource<PerfilPet>(this.ELEMENT_DATA);
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -33,6 +34,10 @@ export class ListComponent implements OnInit {
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+  }
+
+  perfilpetcadastro() {
+    this.router.navigate(['perfilpetcadastro'])
   }
 
 }
--- src/app/components/perfilpetcadastro/perfilpetcadastro.component.ts
+++ src/app/components/perfilpetcadastro/perfilpetcadastro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+import { PerfilpetService } from 'src/app/services/perfilpet.service';
 
 @Component({
   selector: 'app-perfilpetcadastro',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilpetcadastroComponent implements OnInit {
 
-  constructor() { }
+  perfilPetForm: FormGroup;
+
+  validarnome = new FormControl(null);
+  validarraca = new FormControl(null);
+  validaridade = new FormControl(null, [Validators.pattern('^[0-9]*$')]);
+
+  nome: string;
+  raca: string;
+  idade: number;
+  
+  constructor(
+    private perfilPetService: PerfilpetService,
+    private router: Router,
+    private authService: AuthService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.perfilPetForm = new FormGroup({
+      nome: this.validarnome,
+      raca: this.validarraca,
+      idade: this.validaridade
+    });
   }
 
+  incluirPerfilPet(): void {
+    if (this.perfilPetForm.valid) {
+      const idUsuario = this.authService.getUsuarioLogado().id;
+      const perfilPet = this.perfilPetForm.value;
+      perfilPet.usuario = { id: idUsuario };
+      this.perfilPetService.incluir(perfilPet).subscribe(
+        () => {
+          this.toastr.success('Perfil do pet incluído com sucesso!', 'Sucesso');
+          this.router.navigate(['perfilpet']);
+        },
+        error => {
+          this.toastr.error('Por favor, preencha todos os campos corretamente.', 'Erro');
+        }
+      );
+    } else {
+      Object.keys(this.perfilPetForm.controls).forEach(field => {
+        const control = this.perfilPetForm.get(field);
+        control.markAsTouched({ onlySelf: true });
+      });
+    }
+  }
+
+  validaCampos(): boolean {
+    return this.validarnome.valid && this.validarraca.valid && this.validaridade.valid;
+  }
+
+  voltar() {
+    this.router.navigate(['perfilpet'])
+  }
 }
