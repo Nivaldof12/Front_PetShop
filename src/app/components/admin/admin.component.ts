dcf61165031187006d0ca4449092be3e99c890e6
Nivaldof12 <nivaldopvp@gmail.com>
2024-03-11 15:37:12 -0300

    Filtro buscar usuário (Função do Admin)

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Usuario } from 'src/app/models/usuario';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
@@ -11,6 +12,7 @@ export class AdminComponent implements OnInit {
 
   usuarios: Usuario[] = [];
   displayedColumns: string[] = ['position', 'name', 'email', 'password', 'celular'];
+  dataSource = new MatTableDataSource<Usuario>(this.usuarios);
 
   constructor(private usuarioService: UsuarioService) { }
 
@@ -23,11 +25,18 @@ export class AdminComponent implements OnInit {
     this.usuarioService.listarUsuarios().subscribe(
       response => {
         this.usuarios = response;
+        this.dataSource = new MatTableDataSource<Usuario>(this.usuarios);
       },
       error => {
         console.error('Erro ao obter lista de usuários: ', error);
       }
     );
+  }
+
+  // método do filtro da tabela
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
